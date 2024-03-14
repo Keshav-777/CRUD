1623805d12ecb800be7a3c838e41dae25718f062
KESHAV <129521753+Keshav-777@users.noreply.github.com>
2024-03-14 18:32:19 +0530

    Changed the project

--- JDBC/src/Program.java
+++ JDBC/src/Program.java
@@ -1,13 +1,9 @@
-package package1;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 
-public class JDBC1 {
-
+public class Program {
 	static String url = "jdbc:mysql://localhost:3306/jdbc";
 	static String username = "root";
 	static String password = "root";
@@ -25,7 +21,6 @@ public class JDBC1 {
 			stm = con.createStatement(); // SQL statement
 			String query = "select * from employee;";
 			result = stm.executeQuery(query); // execute SQL Query
-			
 			while(result.next()) {
 				System.out.println(result.getInt(1)+" "+result.getString(2)+" "+
 									result.getString(3)+" "+result.getInt(4));
